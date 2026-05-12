aa8156ae77d3ecb56199f5504bcf925630f6e02a
Serbin2 <njh1026@gmail.com>
2026-05-13 01:11:19 +0900

    FEAT : 공격 터렛 메쉬 추가.
    TODO : 상부 메쉬 피봇 등 수정 필요
    전체 콜리젼 크기 조정 필요

--- TowerGame/Source/TowerGame/Public/TGMountedTower.h
+++ TowerGame/Source/TowerGame/Public/TGMountedTower.h
@@ -18,6 +18,18 @@ protected:
 	// 무기 메시
 	UPROPERTY(VisibleAnywhere, Category = "Tower|Base")
 	class UStaticMeshComponent* WeaponMesh;
+	//	무기 방향
+	UPROPERTY(VisibleAnywhere, Category = "Tower|Base")
+	class UArrowComponent* Arrow;
+
+	//	본체 메쉬
+	UPROPERTY(VisibleAnywhere, Category = "Tower|Base")
+	UStaticMeshComponent* BodyMesh;
+
+	//	목표를 가지는 타워라면 해당 목표를 저장할 변수입니다.
+	//	무기 메쉬가 목표를 따라가도록 합니다.
+	UPROPERTY()
+	AActor* Target = nullptr;
 
 	// 런타임에 색상을 변경하기 위한 다이나믹 머티리얼 인스턴스
 	UPROPERTY()
@@ -34,6 +46,10 @@ protected:
 	// 런타임 위젯 인스턴스
 	UPROPERTY()
 	UTGTowerWidget* TowerWidget;
+
+	//	Target이 존재한다면 해당 Target을 따라가도록합니다.
+	//	해당 기능이 필요 없다면 재정의하여 공백으로 만드세요.
+	virtual void FollowTarget();
 
 public:
 	// 업그레이드 레벨 반환
@@ -52,4 +68,6 @@ public:
 
 	// 플레이어가 상호작용 키를 눌렀을 때
 	virtual void OnInteract_Implementation(ATGPlayer* Player) override;
+
+	virtual void Tick(float DeltaTime) override;
 };
